fbe68e57194e44b5f739553c1de4182ace7e2ae3
Davronbek Turaev <T0500796@my.ntu.ac.uk>
2026-04-18 20:54:32 +0100

    Added ConfigManager load on startup and passed config to both RiderMenu and AdminMenu

--- src/RoleSelector.java
+++ src/RoleSelector.java
@@ -3,38 +3,37 @@ import java.util.Scanner;
 public class RoleSelector {
 
     public static void selectRole() {
-    Scanner input = new Scanner(System.in);
-    int menuChoice = 0;
-    System.out.println("\n--- Welcome, CityRide Lite!!! ---");
-    System.out.println("Your daily  travel companion\n");
+        Scanner input = new Scanner(System.in);
 
-    do {
-        // "--- Rider Menu ---"
-        // "Welcome, Rider or name of the rider. If it exists."
+        ConfigManager config = ConfigManager.load();
 
-        System.out.println("\nSelect your role:");
-        System.out.println("1.Rider");
-        System.out.println("2.Admin");
-        System.out.println("3.Exit");
+        int menuChoice = 0;
+        System.out.println("\n--- Welcome, CityRide Lite!!! ---");
+        System.out.println("Your daily travel companion\n");
 
-        System.out.print("\n> ");
-        String choiceText = input.nextLine().trim();
-        try {
-            menuChoice = Integer.parseInt(choiceText);
-        } catch (NumberFormatException e) {
-            System.out.println("INPUT ERROR: Choose a number from 1 to 3");
-            continue;
-        }
+        do {
+            System.out.println("\nSelect your role:");
+            System.out.println("1. Rider");
+            System.out.println("2. Admin");
+            System.out.println("3. Exit");
 
-        switch (menuChoice) {
-            case 1 -> new RiderMenu().run();
-            case 2 -> new AdminMenu().run();
-            case 3 -> System.out.println("\nExit.................");
-            default ->
+            System.out.print("\n> ");
+            String choiceText = input.nextLine().trim();
+            try {
+                menuChoice = Integer.parseInt(choiceText);
+            } catch (NumberFormatException e) {
                 System.out.println("INPUT ERROR: Choose a number from 1 to 3.");
-        }
-    } while (menuChoice != 3);
-    input.close();
-    }
+                continue;
+            }
 
+            switch (menuChoice) {
+                case 1 -> new RiderMenu(config).run();
+                case 2 -> new AdminMenu(config).run();
+                case 3 -> System.out.println("\nGoodbye!");
+                default -> System.out.println("INPUT ERROR: Choose a number from 1 to 3.");
+            }
+
+        } while (menuChoice != 3);
+        input.close();
+    }
 }
