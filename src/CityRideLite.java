057add9b11b9f569b4399f19c25fb0fd44078bce
Davronbek Turaev <T0500796@my.ntu.ac.uk>
2026-04-20 05:35:27 +0100

    All parts of the class names from monolith structure left as comment are cleaned. For clean interface.

--- src/CityRideLite.java
+++ src/CityRideLite.java
@@ -1,26 +1,3 @@
-//final class CityRideDataset {
-//   }
-
-//class Journey {
-//}
-
-
-//class InputValidator {
-//}
-
-//class JourneyManager {
-//
-//}
-
-//class DailySummary {
-//
-//
-//}
-
-// Program menu is in separate class.
-//class Menu {
-//}
-
 public class CityRideLite {
     public static void main(String[] args) {
         FileManager.initialiseDirectories();
